5f6a4f67db34d297f9451d8b692f665ffffd6b4a
JavierRondan <carlosjavier.rondan96@gmail.com>
2024-11-30 09:35:39 -0300

    primer ejercicio

---
+++ clase12/ejercicio1.js
@@ -0,0 +1,36 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+let dia = " ";
+
+switch (dia) {
+    case "lunes" :
+        console.log("Hoy es lunes, empieza la semana con energia.");
+        break;
+    case "martes" :
+        console.log("Hoy es marte, sigue adelante.");
+        break;
+    case "miercoles" :
+        console.log("Hoy es miercoles, mitad de semana.");
+        break;
+    case "jueves" :
+        console.log("Hoy es jueves, casi es viernes.");
+        break;
+    case "viernes":
+        console.log("Hoy es viernes, el fin de semana esta cerca.");
+        break
+    case "sabado" :
+        console.log("Hoy es sabado, disfruta tu dia.");
+        break;
+    case "domingo" :
+        console.log("Hoy es domingo, dia de descanso.");
+        break;
+    default :
+        console.log("Dia no valido.");
+        
+        
+        
+        
+        
+    
+
+}
