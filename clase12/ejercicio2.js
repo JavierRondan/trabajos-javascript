bd7d95da1ba01c5e42d9075c01d0e62e0f7d4fad
JavierRondan <carlosjavier.rondan96@gmail.com>
2024-11-30 09:54:45 -0300

    Segundo ejercicio hecho

---
+++ clase12/ejercicio2.js
@@ -0,0 +1,20 @@
+let dia = "lunes";
+switch (dia) {
+    case "lunes":
+        console.log("tenes clases.");
+        break;
+    case "miercoles":
+        console.log("tenes clases.");
+        break;
+    case "viernes":
+        console.log("tenes clases.");
+        break;
+    default:
+        console.log("no tenes clases.");
+
+        
+                
+
+
+
+}
