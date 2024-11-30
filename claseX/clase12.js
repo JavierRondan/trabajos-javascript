9b18392534f1253fc76899550badb441b3b9d1f5
JavierRondan <carlosjavier.rondan96@gmail.com>
2024-11-30 08:56:54 -0300

    ejercicio hecho en clase

---
+++ claseX/clase12.js
@@ -0,0 +1,8 @@
+let curso = {
+    materias : ["matematicas", "programacion", "economia", "jaja"],
+    horarios : "mañana",
+    alumnos : "chicos/chicas",
+    calificaciones : "trimestrales",
+    asistencia : "asincrona",
+}
+console.log(curso.calificaciones.length)
